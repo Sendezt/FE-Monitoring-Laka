7822b6b8970faad7739e824aec3031a079f50a44
rznive <rezaofc.jr@gmail.com>
2026-08-21 20:22:26 +0700

    NEXT_PUBLIC_API_URL

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.datalakajr.web.id'
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.datalakajr.web.id'
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
